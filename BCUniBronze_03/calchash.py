0ad3388f08f3410fa5daa33f85d115051e7e2090
Nobukazu Tanaka <>
2018-05-29 12:51:35 +0900

    add the anser of exercize03

--- BCUniBronze_03/calchash.py
+++ BCUniBronze_03/calchash.py
@@ -1,18 +1,21 @@
-import hashlib
+# exercise 3
+# 「Blockchain Daigakko」という文字列の後に数字を文字列としてくっ付けて、
+# ハッシュ値の頭に0x00バイトが1個あるまで数字を進めて下さい。その数値を教えて下さい。
+# => 78
+#
+from hashlib import sha256
 
 # test data (integer)
-data = [i for i in range(1, 1000000)]
+data = [i for i in range(1, 10000)]
 
-checkstring : str
+checkstring: str
+
 
 for value in data:
-    checkstring = "Blockchain Daigakko" + str(value)
-    sha256sha256 = hashlib.sha256(hashlib.sha256(checkstring.encode()).digest())
-    decodedhash = hashlib.sha256(hashlib.sha256(checkstring.encode()).digest()).hexdigest()
+    checkstring = 'Blockchain Daigakko' + str(value)
+    # little endian
+    hexdigest = sha256(sha256(checkstring.encode()).digest()).digest()
+    lehexdigest = hexdigest[::-1].hex()
 
-#    print(str(value) + " : " + hashlib.sha256(hashlib.sha256(checkstring.encode()).digest()).hexdigest())
-#    print(str(value) + " : " + str(sha256sha256))
-#    print(str(value) + " : " + str(decodedhash) + " " + str(sha256sha256))
-
-    if "0x00" in str(sha256sha256) or "00" in str(sha256sha256) :
-        print(str(value) + " : " + str(decodedhash) + " " + str(sha256sha256))
+    if lehexdigest[0:2] == '00':
+        print(str(value) + " : " + str(lehexdigest))
